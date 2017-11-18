0304662806540c7e4a19d290decb8d7782e22376
Miha Eržen <miha.erzen1@gmail.com>
2017-11-18 21:44:33 +0100

    Initial index.js file

---
+++ index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { WebView } from 'react-native';
+
+class SaltEdgeConnect extends Component {
+    render() {
+        return (<WebView source='http://miha.website'/>);
+    }
+}
+
+export default SaltEdgeConnect;
